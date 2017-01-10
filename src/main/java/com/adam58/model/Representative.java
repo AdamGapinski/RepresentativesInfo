b8d2e6c319da9821c560d7db7d37a09d8a7b02b2
Adam Gapinski <gapinski.adam@outlook.com>
2017-01-11 00:54:52 +0100

    Modified constructor in Representative

--- src/main/java/com/adam58/model/Representative.java
+++ src/main/java/com/adam58/model/Representative.java
@@ -8,20 +8,36 @@ import java.util.List;
  */
 public class Representative {
     private String name;
+    private String names;
     private String surname;
     private String secondName;
 
     private double totalExpenses;
-    private double minorRenovationExpenses;
+    private double minorRenovationExp;
 
     private List<Integer> termsOfOffice = new ArrayList<>();
     private List<BusinessTrip> trips = new ArrayList<>();
 
     public Representative(RepresentativeDTO representativeDTO) {
         this.name = representativeDTO.data.name;
+        this.names = representativeDTO.data.names;
         this.secondName = representativeDTO.data.secondName;
         this.surname = representativeDTO.data.surname;
 
         this.termsOfOffice = representativeDTO.data.termOfOffice;
+        representativeDTO.layers.expenses.punkty.forEach(tuple -> {
+            this.totalExpenses += tuple.number;
+        });
+
+
+        /*representativeDTO.layers.expenses.punkty
+                .stream()
+                .filter(tuple -> tuple.title.equals("Koszty drobnych napraw i remontów lokalu biura poselskiego"))
+                .forEach(tuple -> {
+                    for (List<Integer> value :
+                            representativeDTO.layers.expenses.values) {
+                        this.minorRenovationExp += value.get(tuple.number - 1);
+                    }
+                });*/
     }
 }
