32dcd50a469766a54c76b0063d4517b5fc48f5ea
Adam Gapinski <gapinski.adam@outlook.com>
2017-01-10 19:50:32 +0100

    Representative.java to model Representative

--- src/main/java/com/adam58/model/Representative.java
+++ src/main/java/com/adam58/model/Representative.java
@@ -1,8 +1,18 @@
 package com.adam58.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author Adam Gapiński
  */
 public class Representative {
+    private String name;
+    private String surname;
 
+    private double totalExpenses;
+    private double minorRenovationExpenses;
+
+    private List<Integer> termsOfOffice = new ArrayList<>();
+    private List<BusinessTrip> trips = new ArrayList<>();
 }
