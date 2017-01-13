52e4dd14460e84c1a6ec2c1411dada7e2f0e6186
Adam Gapinski <gapinski.adam@outlook.com>
2017-01-13 03:47:12 +0100

    Implemented Representatives data model

--- src/main/java/com/adam58/model/RepresentativesDataModel.java
+++ src/main/java/com/adam58/model/RepresentativesDataModel.java
@@ -2,40 +2,80 @@ package com.adam58.model;
 
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.function.BinaryOperator;
+import java.util.stream.Collectors;
 
 /**
  * @author Adam Gapiński
  */
 public class RepresentativesDataModel implements IRepresentativesDataModel {
     List<Representative> representatives = new CopyOnWriteArrayList<>();
+    IFetchRepresentativesData dataProvider = new FetchRepresentativesFromWebApi();
+
 
     @Override
-    public Representative getRep(String name) {
+    public Representative getRepresentative(String name, String surname) {
+        try {
+            return dataProvider.fetchRepresentativeByName(name, surname);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         return null;
+    }
+
+    @Override
+    public Representative getRepresentative(String name, String secondName, String surname) {
+        try {
+            return dataProvider.fetchRepresentativeByName(name, secondName, surname);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+
+    private Representative getRepMostOf(int termOfOffice, BinaryOperator<Representative> operator) {
+        return dataProvider.fetchRepresentativesByTermOfOffice(termOfOffice)
+                .stream()
+                .reduce(operator)
+                .get();
     }
 
     @Override
     public Representative getRepMostTrips(int termOfOffice) {
-        return null;
+        return getRepMostOf(termOfOffice, (representative, representative2) ->
+                (representative.getBusinessTripsCount() > representative2.getBusinessTripsCount()) ?
+                representative : representative2);
     }
 
     @Override
     public Representative getRepLongestTripsResidency(int termOfOffice) {
-        return null;
+        return getRepMostOf(termOfOffice, (representative, representative2) ->
+                (representative.calculateTotalBusinessTripsResidency() >
+                        representative2.calculateTotalBusinessTripsResidency()) ?
+                        representative : representative2);
     }
 
     @Override
     public Representative getRepMostExpensiveTrip(int termOfOffice) {
-        return null;
+        return getRepMostOf(termOfOffice, (representative, representative2) ->
+                (representative.calculateMostExpensiveTripPrice() > representative2.calculateMostExpensiveTripPrice()) ?
+                        representative : representative2);
     }
 
     @Override
     public List<Representative> getRepsByTermOfOffice(int termOfOffice) {
-        return null;
+        return dataProvider.fetchRepresentativesByTermOfOffice(termOfOffice);
     }
 
     @Override
     public List<Representative> getRepsByTripDestination(String destination, int termOfOffice) {
-        return null;
+
+        return dataProvider.fetchRepresentativesByTermOfOffice(termOfOffice)
+                .stream()
+                .filter(representative -> representative.wereOnBusinessTripIn("Włochy"))
+                .collect(Collectors.toList());
     }
 }
