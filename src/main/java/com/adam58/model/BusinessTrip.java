76f588f25e22948fd429a67f798c9cf92e82cc8c
Adam Gapinski <gapinski.adam@outlook.com>
2017-01-13 03:46:15 +0100

    Implemented Representative data model

--- src/main/java/com/adam58/model/BusinessTrip.java
+++ src/main/java/com/adam58/model/BusinessTrip.java
@@ -4,4 +4,32 @@ package com.adam58.model;
  * @author Adam Gapiński
  */
 public class BusinessTrip {
+    private String country;
+    private int days;
+    private double totalExpense;
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public void setDays(int days) {
+        this.days = days;
+    }
+
+    public void setTotalExpense(Double totalExpense) {
+        this.totalExpense = totalExpense;
+    }
+
+
+    public String getCountry() {
+        return country;
+    }
+
+    public int getDays() {
+        return days;
+    }
+
+    public Double getTotalExpense() {
+        return totalExpense;
+    }
 }
--- src/main/java/com/adam58/model/Representative.java
+++ src/main/java/com/adam58/model/Representative.java
@@ -8,36 +8,105 @@ import java.util.List;
  */
 public class Representative {
     private String name;
-    private String names;
     private String surname;
     private String secondName;
+    private String nameDopelniacz;
 
     private double totalExpenses;
     private double minorRenovationExp;
 
-    private List<Integer> termsOfOffice = new ArrayList<>();
     private List<BusinessTrip> trips = new ArrayList<>();
+
+    public Representative(){}
 
     public Representative(RepresentativeDTO representativeDTO) {
         this.name = representativeDTO.data.name;
-        this.names = representativeDTO.data.names;
         this.secondName = representativeDTO.data.secondName;
         this.surname = representativeDTO.data.surname;
 
-        this.termsOfOffice = representativeDTO.data.termOfOffice;
-        representativeDTO.layers.expenses.punkty.forEach(tuple -> {
-            this.totalExpenses += tuple.number;
-        });
+        representativeDTO.layers.expenses.punkty.forEach(tuple -> this.totalExpenses += tuple.number);
+    }
 
+    @Override
+    public String toString() {
+        if (secondName == null) {
+            return String.format("%s %s", this.getName(), this.getSurname());
+        } else {
+            return String.format("%s %s %s", this.getName(), this.getSecondName(), this.getSurname());
+        }
+    }
 
-        /*representativeDTO.layers.expenses.punkty
-                .stream()
-                .filter(tuple -> tuple.title.equals("Koszty drobnych napraw i remontów lokalu biura poselskiego"))
-                .forEach(tuple -> {
-                    for (List<Integer> value :
-                            representativeDTO.layers.expenses.values) {
-                        this.minorRenovationExp += value.get(tuple.number - 1);
-                    }
-                });*/
+    public boolean wereOnBusinessTripIn(String destination) {
+        return trips.stream()
+                .anyMatch(businessTrip -> businessTrip
+                        .getCountry()
+                        .toLowerCase()
+                        .equals(destination.toLowerCase()));
+    }
+
+    public int calculateTotalBusinessTripsResidency() {
+        return trips.stream().mapToInt(BusinessTrip::getDays).sum();
+    }
+
+    public double calculateMostExpensiveTripPrice() {
+        return trips.stream()
+                .mapToDouble(BusinessTrip::getTotalExpense)
+                .reduce(0, (left, right) -> left > right ? left : right);
+    }
+
+    public int getBusinessTripsCount() {
+        return trips.size();
+    }
+
+    public void addBusinessTrip(BusinessTrip businessTrip) {
+        trips.add(businessTrip);
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public String getNameDopelniacz() {
+        return nameDopelniacz;
+    }
+
+    public void setNameDopelniacz(String nameDopelniacz) {
+        this.nameDopelniacz = nameDopelniacz;
+    }
+
+    public void setTotalExpenses(double totalExpenses) {
+        this.totalExpenses = totalExpenses;
+    }
+
+    public void setMinorRenovationExp(double minorRenovationExp) {
+        this.minorRenovationExp = minorRenovationExp;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public double getTotalExpenses() {
+        return totalExpenses;
+    }
+
+    public double getMinorRenovationExp() {
+        return minorRenovationExp;
     }
 }
